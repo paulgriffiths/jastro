c22f6a75c77fe87a2840cc54dcf0eab691eb64b9
Paul Griffiths <mail@paulgriffiths.net>
2015-04-11 20:27:33 -0400

    Add hours, minutes and seconds class.

---
+++ src/astro/HMS.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package astro;
+
+import java.util.Formatter;
+
+/**
+ * Hours, minutes and seconds class.
+ * 
+ * @author Paul Griffiths
+ */
+public class HMS {
+    
+    /**  Seconds in a day constant  */
+    private static final int SECS_IN_A_DAY = 86400;
+    
+    /**  Seconds in an hour constant  */
+    private static final int SECS_IN_A_HOUR = 3600;
+    
+    /**  Seconds in a minute constant  */
+    private static final int SECS_IN_A_MIN = 60;
+    
+    /*  Angle in normalized degrees  */
+    private final double degrees;
+    
+    /*  Integral number of hours  */
+    private final int hours;
+    
+    /*  Integral number of minutes  */
+    private final int minutes;
+    
+    /*  Integral number of seconds  */
+    private final int seconds;
+    
+    /**
+     * Class constructor taking angle in degrees.
+     * 
+     * Degrees will be normalized, i.e. put in the range
+     * {@literal 0 <= degrees < 360}
+     * 
+     * @param degrees   The angle in degrees
+     */
+    public HMS(final double degrees) {
+        this.degrees = Astro.normalizeDegrees(degrees);
+        final long totalSecs = Math.round(this.degrees / 360 * SECS_IN_A_DAY);
+        hours = (int) (totalSecs / SECS_IN_A_HOUR);
+        minutes = (int) ((totalSecs - hours * SECS_IN_A_HOUR) /
+                          SECS_IN_A_MIN);
+        seconds = (int) (totalSecs - hours * SECS_IN_A_HOUR -
+                                     minutes * SECS_IN_A_MIN);
+    }
+    
+    /**
+     * Default class constructor.
+     * 
+     * Initializes angle to zero degrees.
+     */
+    public HMS() {
+        this(0.0);
+    }
+    
+    /**
+     * Returns the integral number of hours.
+     * 
+     * @return  The integeral number of hours 
+     */
+    public int getHours() {
+        return hours;
+    }
+    
+    /**
+     * Returns the integral number of minutes.
+     * 
+     * @return  The integral number of minutes 
+     */
+    public int getMinutes() {
+        return minutes;
+    }
+    
+    /**
+     * Returns the integral number of seconds.
+     * 
+     * @return  The integral number of seconds 
+     */
+    public int getSeconds() {
+        return seconds;
+    }
+    
+    /**
+     * Returns the angle in degrees.
+     * 
+     * @return  The angle in degrees 
+     */
+    public double toDegrees() {
+        return degrees;
+    }
+    
+    /**
+     * Returns the angle in radians.
+     * 
+     * @return  The angle in radians 
+     */
+    public double toRadians() {
+        return Math.toRadians(toDegrees());
+    }
+    
+    /**
+     * Returns a string representation in the form "00h 00m 00s".
+     * 
+     * @return  A string representation of the hours, minutes and seconds 
+     */
+    @Override
+    public String toString() {
+        Formatter fmt = new Formatter();
+        fmt.format("%02dh %02dm %02ds", hours, minutes, seconds);
+        return fmt.toString();
+    }
+}
---
+++ test/tests/HMSTest.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import astro.HMS;
+
+/**
+ * Hours, minutes and seconds class unit tests class.
+ * 
+ * @author Paul Griffiths
+ */
+public class HMSTest {
+    public HMSTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testGetHMS() {
+        HMS hms = new HMS();
+        assertEquals(0, hms.getHours());
+        assertEquals(0, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(90.0);
+        assertEquals(6, hms.getHours());
+        assertEquals(0, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(100.0);
+        assertEquals(6, hms.getHours());
+        assertEquals(40, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(107.4);
+        assertEquals(7, hms.getHours());
+        assertEquals(9, hms.getMinutes());
+        assertEquals(36, hms.getSeconds());
+        
+        hms = new HMS(110.0);
+        assertEquals(7, hms.getHours());
+        assertEquals(20, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(360.0);
+        assertEquals(0, hms.getHours());
+        assertEquals(0, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+     
+        hms = new HMS(389.62);
+        assertEquals(1, hms.getHours());
+        assertEquals(58, hms.getMinutes());
+        assertEquals(29, hms.getSeconds());
+     
+        hms = new HMS(450.0);
+        assertEquals(6, hms.getHours());
+        assertEquals(0, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(-100.0);
+        assertEquals(17, hms.getHours());
+        assertEquals(20, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(-107.4);
+        assertEquals(16, hms.getHours());
+        assertEquals(50, hms.getMinutes());
+        assertEquals(24, hms.getSeconds());
+        
+        hms = new HMS(-110.0);
+        assertEquals(16, hms.getHours());
+        assertEquals(40, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+        
+        hms = new HMS(-360.0);
+        assertEquals(0, hms.getHours());
+        assertEquals(0, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+     
+        hms = new HMS(-389.62);
+        assertEquals(22, hms.getHours());
+        assertEquals(1, hms.getMinutes());
+        assertEquals(31, hms.getSeconds());
+     
+        hms = new HMS(-450.0);
+        assertEquals(18, hms.getHours());
+        assertEquals(0, hms.getMinutes());
+        assertEquals(0, hms.getSeconds());
+    }
+    
+    @Test
+    public void testToString() {
+        HMS hms = new HMS(90.0);
+        assertEquals(hms.toString(), "06h 00m 00s");
+        
+        hms = new HMS(-90.0);
+        assertEquals(hms.toString(), "18h 00m 00s");
+        
+        hms = new HMS(389.62);
+        assertEquals(hms.toString(), "01h 58m 29s");
+     
+        hms = new HMS(-389.62);
+        assertEquals(hms.toString(), "22h 01m 31s");
+    }
+}
